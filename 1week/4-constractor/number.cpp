94c888c7b55f949a28ec6e01a2ff26acca6f593e
luca3104 <s1210006@u-aizu.ac.jp>
2016-10-04 01:24:57 +0900

    4週間目を終わらす

---
+++ 1week/4-constractor/number.cpp
@@ -0,0 +1,18 @@
+#include "number.h"
+
+Number::Number() :a(0),b(0)
+{}
+
+Number::~Number()
+{}
+
+//  二つの数値を設定
+void Number::setNumbers(int n1, int n2)
+{
+    a = n1;
+    b = n2;
+}
+int Number::getAdd()
+{
+    return a + b;
+}
